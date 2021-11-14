022ef7cbfe66696bc5dc0140b04b6ca4b3d617ed
nayeemhust <nayeemhust@gmail.com>
2021-11-14 22:49:30 +0600

    Initial Commit

---
+++ Easy Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Nayeem Nazmul on 2021/11/14.
+//  Copyright © 2021 Nayeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
